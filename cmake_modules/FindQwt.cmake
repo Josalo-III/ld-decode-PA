6b73dd36333309f120ffdc637dc808ba0bc79ea7
Joseph Burns <josalo@josalo.com>
2025-09-07 19:42:40 -0700

    Update CMakeLists.txt for Qt6

--- cmake_modules/FindQwt.cmake
+++ cmake_modules/FindQwt.cmake
@@ -1,57 +1,67 @@
-# Find Qwt
-# ~~~~~~~~
-# Copyright (c) 2010, Tim Sutton <tim at linfiniti.com>
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
-#
-# Once run this will define:
-#
-# QWT_FOUND       = system has QWT lib
-# QWT_LIBRARY     = full path to the QWT library
-# QWT_INCLUDE_DIR = where to find headers
-#
-
-
-set(QWT_LIBRARY_NAMES qwt-qt5 qwt6-qt5 qwt qwt6)
-
-find_library(QWT_LIBRARY
-  NAMES ${QWT_LIBRARY_NAMES}
-  PATHS
-    /usr/lib
-    /usr/local/lib
-    /usr/local/lib/qt5
-    "$ENV{LIB_DIR}/lib"
-    "$ENV{LIB}"
-)
-
-set(_qwt_fw)
-if(QWT_LIBRARY MATCHES "/qwt.*\\.framework")
-  string(REGEX REPLACE "^(.*/qwt.*\\.framework).*$" "\\1" _qwt_fw "${QWT_LIBRARY}")
+if(Qwt_FOUND)
+  return()
 endif()
 
-FIND_PATH(QWT_INCLUDE_DIR NAMES qwt.h PATHS
-  "${_qwt_fw}/Headers"
-  /usr/include
-  /usr/include/qt5
-  /usr/local/include
-  /usr/local/include/qt5
-  "$ENV{LIB_DIR}/include"
-  "$ENV{INCLUDE}"
-  PATH_SUFFIXES qwt-qt5 qwt qwt6 qt5/qwt
-)
+if(DEFINED QWT_ROOT AND NOT DEFINED Qwt_ROOT)
+  set(Qwt_ROOT "${QWT_ROOT}")
+endif()
 
-IF (QWT_INCLUDE_DIR AND QWT_LIBRARY)
-  SET(QWT_FOUND TRUE)
-ENDIF (QWT_INCLUDE_DIR AND QWT_LIBRARY)
+set(_QWT_HINTS)
+if(Qwt_ROOT)
+  list(APPEND _QWT_HINTS "${Qwt_ROOT}" "${Qwt_ROOT}/lib" "${Qwt_ROOT}/Library/Frameworks" "${Qwt_ROOT}/Frameworks")
+endif()
+# Always ensure our known prefix first
+list(INSERT _QWT_HINTS 0 /usr/local/qwt-6.3.0/lib /usr/local/qwt-6.3.0)
 
-IF (QWT_FOUND)
-  FILE(READ ${QWT_INCLUDE_DIR}/qwt_global.h qwt_header)
-  STRING(REGEX REPLACE "^.*QWT_VERSION_STR +\"([^\"]+)\".*$" "\\1" QWT_VERSION_STR "${qwt_header}")
-  IF (NOT QWT_FIND_QUIETLY)
-    MESSAGE(STATUS "Found Qwt: ${QWT_LIBRARY} (${QWT_VERSION_STR})")
-  ENDIF (NOT QWT_FIND_QUIETLY)
-ELSE (QWT_FOUND)
-  IF (QWT_FIND_REQUIRED)
-    MESSAGE(FATAL_ERROR "Could not find Qwt")
-  ENDIF (QWT_FIND_REQUIRED)
-ENDIF (QWT_FOUND)
+set(QWT_LIBRARY "")
+set(QWT_INCLUDE_DIRS "")
+
+# Framework search
+foreach(_h IN LISTS _QWT_HINTS)
+  if(EXISTS "${_h}/qwt.framework")
+    set(_fw "${_h}/qwt.framework")
+    if(EXISTS "${_fw}/Versions/6/Headers")
+      list(APPEND QWT_INCLUDE_DIRS "${_fw}/Versions/6/Headers")
+    elseif(EXISTS "${_fw}/Versions/Current/Headers")
+      list(APPEND QWT_INCLUDE_DIRS "${_fw}/Versions/Current/Headers")
+    elseif(EXISTS "${_fw}/Headers")
+      list(APPEND QWT_INCLUDE_DIRS "${_fw}/Headers")
+    endif()
+
+    if(EXISTS "${_fw}/Versions/6/qwt")
+      set(QWT_LIBRARY "${_fw}/Versions/6/qwt")
+    elseif(EXISTS "${_fw}/qwt")
+      set(QWT_LIBRARY "${_fw}/qwt")
+    endif()
+
+    if(QWT_LIBRARY AND QWT_INCLUDE_DIRS)
+      break()
+    endif()
+  endif()
+endforeach()
+
+# Fallback (classic lib) not needed; skip unless you want:
+# if(NOT QWT_LIBRARY) find_library(QWT_LIBRARY NAMES qwt) endif()
+
+# Version (optional)
+if(QWT_INCLUDE_DIRS AND EXISTS "${QWT_INCLUDE_DIRS}/qwt_global.h")
+  file(STRINGS "${QWT_INCLUDE_DIRS}/qwt_global.h" _v REGEX "#define[ \t]+QWT_VERSION_STR")
+  if(_v MATCHES "#define[ \t]+QWT_VERSION_STR[ \t]+\"([0-9\\.]+)\"")
+    set(QWT_VERSION "${CMAKE_MATCH_1}")
+  endif()
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Qwt REQUIRED_VARS QWT_LIBRARY QWT_INCLUDE_DIRS VERSION_VAR QWT_VERSION)
+
+if(Qwt_FOUND AND NOT TARGET Qwt::Qwt)
+  add_library(Qwt::Qwt INTERFACE IMPORTED)
+  set_target_properties(Qwt::Qwt PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${QWT_INCLUDE_DIRS}"
+    INTERFACE_LINK_LIBRARIES "${QWT_LIBRARY}"
+  )
+  message(STATUS "Using Qwt at: ${QWT_LIBRARY}")
+  message(STATUS "Qwt includes: ${QWT_INCLUDE_DIRS}")
+endif()
+
+mark_as_advanced(QWT_LIBRARY QWT_INCLUDE_DIRS)
